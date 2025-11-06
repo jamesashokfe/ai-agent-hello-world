61a3b046d4ec6bb11d2dc042e333648d84d65ab4
James Ashok <jamesashok93+github@gmail.com>
2025-11-06 18:51:58 +0530

    Hello world AI agent.

---
+++ hello_world_agent.py
@@ -0,0 +1,32 @@
+import os
+from openai import OpenAI, api_key
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+api_key = os.getenv("OPENROUTER_API_KEY")
+
+if not api_key:
+    raise ValueError("API key not found in environment variables")
+
+# Initialize OpenAI client
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key=api_key,
+)
+
+# Prompt
+user_query = input("User: ")
+
+# Send query and get response
+response = client.chat.completions.create(
+  model="openai/gpt-oss-20b:free",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": user_query}
+  ]
+)
+
+# Show response
+ai_reply = response.choices[0].message.content
+print("AI agent: ", ai_reply)
